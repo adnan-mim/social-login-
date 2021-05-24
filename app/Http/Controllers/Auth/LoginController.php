e61d932cc8b3b325fc15f18ceb5965eb40ad4f91
adnan-mim <adnanmim873@outlook.com>
2021-05-24 16:01:33 +0600

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Socialite;
+
+class LoginController extends Controller
+{
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::with('kakao')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('kakao')->user();
+
+        dd($user);
+
+        // $user->token;
+    }
+}
